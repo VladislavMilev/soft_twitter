7982d9107f239ca2c46691faf4b317d172e82fd8
VladislavMilev <jimroot555@gmail.com>
2019-08-02 17:20:29 +0300

    test commit

--- src/router.py
+++ src/router.py
@@ -211,3 +211,7 @@ def sign_out():
     session.pop('user_login', None)
     session.pop('user_role', None)
     return redirect(url_for('login'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
